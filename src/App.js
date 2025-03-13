d26a22373922004af183d5c17276dd69976dae84
Saathvik10 <67659075+Saathvik10@users.noreply.github.com>
2025-03-13 13:54:23 -0500

    init react app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Pokemon Stats</h1>
+      </header>
+      <main>
+        {/* Pokemon content will go here */}
+      </main>
+    </div>
+  );
+}
+
+export default App; 
